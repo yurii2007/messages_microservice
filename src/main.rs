4ff67909c1213aaf36ae8a0f92f3c179889121e9
yurii2007 <yurash862@gmail.com>
2024-08-24 10:59:23 +0300

    added http server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+extern crate hyper;
+extern crate futures;
+
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+
+use hyper::server::{ Request, Response, Service };
+use futures::future::Future;
+
+struct MicroService;
+
+impl Service for MicroService {
+    type Request = Request;
+    type Response = Response;
+    type Error = hyper::Error;
+    type Future = Box<dyn Future<Item = Self::Response, Error = Self::Error>>;
+
+    fn call(&self, req: Request) -> Self::Future {
+        info!("Microservice received a request {:?}", req);
+        Box::new(futures::future::ok(Response::new().with_body("hello world")))
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+    let address = "127.0.0.1:8080".parse().unwrap();
+    let server = hyper::server::Http
+        ::new()
+        .bind(&address, || Ok(MicroService {}))
+        .unwrap();
+
+    info!("Running microservice at {}", address);
+    server.run().unwrap();
 }
